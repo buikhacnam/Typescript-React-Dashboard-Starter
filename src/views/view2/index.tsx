ff3c35f9bf4362135f3f1d8a258c3b4a83608666
Bui Nam <buikhacnam11@gmail.com>
2022-03-22 16:27:58 +0700

    update

--- src/views/view2/index.tsx
+++ src/views/view2/index.tsx
@@ -5,39 +5,39 @@ import {getToken, onMessageListener} from '../../firebaseInit'
 interface view2Props {}
 
 const View2: React.FC<view2Props> = ({}) => {
-    const [isTokenFound, setTokenFound] = React.useState(false);
-	const [show, setShow] = React.useState(false);
-	const [notification, setNotification] = React.useState({ title: "", body: "" });
-	// console.log("Token found", isTokenFound);
+    // const [isTokenFound, setTokenFound] = React.useState(false);
+	// const [show, setShow] = React.useState(false);
+	// const [notification, setNotification] = React.useState({ title: "", body: "" });
+	// // console.log("Token found", isTokenFound);
 
-	React.useEffect(() => {
-		let data;
+	// React.useEffect(() => {
+	// 	let data;
 	
-		async function tokenFunc() {
-		  data = await getToken(setTokenFound);
-		  console.log('data', data)
-		  if (data) {
-			console.log("Token is", data);
-		  }
-		  return data;
-		}
+	// 	async function tokenFunc() {
+	// 	  data = await getToken(setTokenFound);
+	// 	  console.log('data', data)
+	// 	  if (data) {
+	// 		console.log("Token is", data);
+	// 	  }
+	// 	  return data;
+	// 	}
 	
-		tokenFunc();
-	}, [setTokenFound]);
+	// 	tokenFunc();
+	// }, [setTokenFound]);
 
 
-    onMessageListener()
-    .then((payload) => {
-      setShow(true);
-      setNotification({
-        title: payload.notification.title,
-        body: payload.notification.body,
-      });
-		console.log('hiiiiiiiiiiiiiiiiii')
-      console.log(payload);
-	  message.success(payload.notification.title)
-    })
-    .catch((err) => console.log("failed: ", err));
+    // onMessageListener()
+    // .then((payload) => {
+    //   setShow(true);
+    //   setNotification({
+    //     title: payload.notification.title,
+    //     body: payload.notification.body,
+    //   });
+	// 	console.log('hiiiiiiiiiiiiiiiiii')
+    //   console.log(payload);
+	//   message.success(payload.notification.title)
+    // })
+    // .catch((err) => console.log("failed: ", err));
     
 
     return (
